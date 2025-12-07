5d08e88b1b91c28a8655e17f1dc761485c91cfb4
Gaurav R <gaurav77955@gmail.com>
2025-12-07 20:10:05 +0530

    Change database backend from MySQL to PostgreSQL

--- medicare/settings.py
+++ medicare/settings.py
@@ -93,17 +93,13 @@ WSGI_APPLICATION = 'medicare.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'medicare'),
-        'USER': os.getenv('DB_USER', 'medicare_user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'medicare_pass'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '3306'),
-        'CONN_MAX_AGE': 0,
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
